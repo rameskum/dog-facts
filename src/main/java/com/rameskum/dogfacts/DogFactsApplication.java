5a05ede346e7564c0ba8b3fbf09d6cb345883f24
rameskum <rameskum.ms@outlook.com>
2022-03-26 18:03:13 +0530

    initial commit

---
+++ src/main/java/com/rameskum/dogfacts/DogFactsApplication.java
@@ -0,0 +1,13 @@
+package com.rameskum.dogfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogFactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogFactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rameskum/dogfacts/DogFactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rameskum.dogfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogFactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
