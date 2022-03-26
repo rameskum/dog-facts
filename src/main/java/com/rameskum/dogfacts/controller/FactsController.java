39ea04888ee48226bcb477f4ea0563edcee2c916
rameskum <rameskum.ms@outlook.com>
2022-03-26 20:08:21 +0530

    adding tests

--- src/main/java/com/rameskum/dogfacts/controller/FactsController.java
+++ src/main/java/com/rameskum/dogfacts/controller/FactsController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/dog/facts")
+@RequestMapping("/api/v1/facts")
 @RestController
 public class FactsController {
 
 	private final FactService factService;
 
-	@GetMapping
+	@GetMapping(value = "dog", produces = "application/json")
 	public ResponseEntity<FactResponse> facts(@RequestParam(value = "count", defaultValue = "1") int count) {
 		return ResponseEntity.ok(factService.getFacts(count));
 	}
--- src/test/java/com/rameskum/dogfacts/DogFactsApplicationTests.java
+++ src/test/java/com/rameskum/dogfacts/DogFactsApplicationTests.java
@@ -1,13 +1,50 @@
 package com.rameskum.dogfacts;
 
+import com.rameskum.dogfacts.controller.FactsController;
+import com.rameskum.dogfacts.service.FactService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DogFactsApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private FactsController controller;
+
+	@Autowired
+	private FactService factService;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
+	@Test
+	void response() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/facts/dog"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+	}
+
+	@Test
+	void testFactService() {
+		Assertions.assertEquals(1, this.factService.getFacts(1).getFacts().size());
+		Assertions.assertEquals(2, this.factService.getFacts(2).getFacts().size());
+		Assertions.assertEquals(20, this.factService.getFacts(20).getFacts().size());
+	}
 }
